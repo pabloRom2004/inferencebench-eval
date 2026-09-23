636694ed799e01c2d1ff1bef58b477f4237d6f79
pabloRom2004 <pablo2004romero@gmail.com>
2026-09-23 18:38:18 +0100

    Add a GPU-free deployment probe

--- src/inferencebench/__init__.py
+++ src/inferencebench/__init__.py
@@ -1,5 +1,6 @@
+from inferencebench.diagnostics import provider_probe
 from inferencebench.harness_default import cli_agent, default_agent, react_agent
 from inferencebench.harness_original import original_agent
 from inferencebench.task import inference_bench
 
-__all__ = ["inference_bench", "original_agent", "react_agent", "cli_agent", "default_agent"]
+__all__ = ["provider_probe", "inference_bench", "original_agent", "react_agent", "cli_agent", "default_agent"]
